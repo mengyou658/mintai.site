a72fe962db2a7643fff456bfdaf421220ea8f776
root <root@AY130407222459530f18.(none)>
2013-05-20 18:38:12 +0800

    init

--- application/views/signin.php
+++ application/views/signin.php
@@ -2,7 +2,7 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<title>听说后台管理系统</title>
+		<title>民泰后台管理系统</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="">
 		<meta name="author" content="">
